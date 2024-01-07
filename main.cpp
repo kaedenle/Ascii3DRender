73cf3953ae3cc5a73de686ef0f0c157f968646a7
Kaeden Le <kaedenle@gmail.com>
2024-01-07 23:58:04 +0800

    Added easier way to change shapes

--- main.cpp
+++ main.cpp
@@ -16,23 +16,23 @@ float addToAngle(float, float);
 int main(){
     CoordinateSpace cs(12, 12);
     //cube
-    /*vector<coords3D> coords{{5, 5, 5}, {-5, 5, 5}, {-5, -5, 5}, {5, -5, 5}, {5, 5, -5}, {-5, 5, -5}, {-5, -5, -5}, {5, -5, -5}};
+    vector<coords3D> coords{{5, 5, 5}, {-5, 5, 5}, {-5, -5, 5}, {5, -5, 5}, {5, 5, -5}, {-5, 5, -5}, {-5, -5, -5}, {5, -5, -5}};
     vector<EdgeDef> edges{{0, 1}, {1, 2}, {2, 3}, {0, 3}, {0, 4}, {1, 5}, {2, 6}, {3, 7}, {4, 5}, {5, 6}, {6, 7}, {4, 7}};
     vector<Face> faces{Face(::FaceType::forward, vector<int>{0, 1, 2, 3}), Face(::FaceType::right, vector<int>{4, 3, 7, 11}), Face(::FaceType::back, vector<int>{8, 11, 10, 9}), 
                         Face(::FaceType::left, vector<int>{5, 9, 6, 1}), Face(::FaceType::top, vector<int>{8, 4, 0, 5}), Face(::FaceType::bottom, vector<int>{2, 6, 10, 7})};
     Shape cube(coords, 
             edges,
-            faces);*/
+            faces);
     
     //pyramids
-    /*vector<coords3D> coords2{{-6, -6, -6}, {6, -6, -6}, {-6, -6, 6}, {6, -6, 6}, {0, 8, 0}};
+    vector<coords3D> coords2{{-6, -6, -6}, {6, -6, -6}, {-6, -6, 6}, {6, -6, 6}, {0, 8, 0}};
     vector<EdgeDef> edges2{{0, 1}, {2, 3}, {0, 2}, {1, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}};
     vector<Face> faces2{Face(::FaceType::back, vector<int>{0, 4, 5}), Face(::FaceType::right, vector<int>{3, 5, 7}), 
                         Face(::FaceType::forward, vector<int>{1, 6, 7}), Face(::FaceType::left, vector<int>{2, 4, 6}), 
                         Face(::FaceType::bottom, vector<int>{0, 1, 2, 3})};
     Shape pyramid(coords2, 
             edges2,
-            faces2);*/
+            faces2);
 
     //hexagonal pyramid
     vector<coords3D> coords3{{{-4, 6, 0}, {4, 6, 0}, {6, 0, 0}, {4, -6, 0}, {-4, -6, 0}, {-6, 0, 0}, {0, 0, 8}}};
@@ -43,6 +43,8 @@ int main(){
     Shape hexagon(coords3, 
             edges3,
             faces3);
+
+    Shape &s = hexagon;
 
     bool pressing[7]{ 0 };
     bool hasPressed = TRUST_E_NO_SIGNER_CERT;
@@ -84,7 +86,7 @@ int main(){
         if(hasPressed){
             hasPressed = false;
             fill(begin(pressing), end(pressing), false);
-            reRender(cs, hexagon, x_angle, y_angle, z_angle);
+            reRender(cs, s, x_angle, y_angle, z_angle);
             Sleep(20);
         }
     }
